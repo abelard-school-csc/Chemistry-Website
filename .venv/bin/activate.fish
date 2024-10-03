5e146597e0f58635a62d91d52f766c9364d9c2b0
abelard-school-csc <amuratov@abelardschool.org>
2024-10-03 13:30:06 -0400

    fixed reproducability issue

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/andrew/School/Compsci/Chemistry/.venv"
+set -gx VIRTUAL_ENV "/home/andrew/Documents/Chemistry/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
